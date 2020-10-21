30ac55dda55f5b3c947cdcca10bed09f96350b36
abdulhaseeb13mar <abdulhaseeb13mar@gmail.com>
2020-10-21 23:54:29 +0500

    first deployment

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,11 +1,48 @@
-import React from 'react'
+import React, { useState, useEffect } from "react";
+import { Grid, Avatar, Typography, IconButton } from "@material-ui/core";
+import { makeStyles } from "@material-ui/core/styles";
+import Zoom from "react-reveal/Zoom";
+import ME from "../../assets/me.jpg";
+import "./Home.scss";
 
 function Home() {
-    return (
-        <div>
-            Home
+  useEffect(() => {
+    makeTrue();
+  }, []);
+  const [show, setShow] = useState(false);
+  const classes = useStyles();
+
+  const makeTrue = () => {
+    setTimeout(() => {
+      setShow(true);
+    }, 200);
+  };
+  return (
+    <Grid className="main-home-wrapper" container>
+      <Zoom right cascade when={show}>
+        <div className="home-centered-div" onClick={() => setShow(false)}>
+          <Avatar alt="Me" src={ME} className={classes.large} />
+          <Typography variant="h3" className="home-text1">
+            Hey! <br /> I am Abdul Haseeb
+          </Typography>
         </div>
-    )
+      </Zoom>
+    </Grid>
+  );
 }
 
-export default Home
+const useStyles = makeStyles((theme) => ({
+  large: {
+    width: theme.spacing(30),
+    height: theme.spacing(30),
+    boxShadow: "0px 0px 18px 8px black",
+    transition: "0.2s ease-in-out",
+    cursor: "pointer",
+    "&:hover": {
+      width: theme.spacing(35),
+      height: theme.spacing(35),
+    },
+  },
+}));
+
+export default Home;
